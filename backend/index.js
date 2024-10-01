399f81f54c23641e48e7cc3a904b1541f450e7fc
moneywar <kong.deesamoot@gmail.com>
2024-10-01 16:22:58 +0700

    add file_name and change music gen path

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ app.get('/api/music/ai', (req, res) => {
     // Use the Python executable from the virtual environment
     const pythonScriptPath = path.join('../ai/', 'interface.py');
     const pythonExecutable = 'python'; // Path to Python in the virtual environment
-    const pythonProcess = spawn(pythonExecutable, [pythonScriptPath, 1, "jrpg", "piano"]);
+    const pythonProcess = spawn(pythonExecutable, [pythonScriptPath, 1, "jrpg", "saxophone"]);
     let output = '';
     pythonProcess.stdout.on('data', (data) => {
         output += data.toString();
